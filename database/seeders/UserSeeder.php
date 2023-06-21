1693b2c166c605a8557889b8083006530f35a75f
frannoi12 <francoistoyi4@gmail.com>
2023-06-21 10:25:59 +0000

    décommentation

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,9 +6,9 @@ use App\Models\User;
 //use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+
 //use Spatie\Permission\Models\Role;
 //use Spatie\Permission\Models\Permission;
-
 
 class UserSeeder extends Seeder
 {
@@ -19,21 +19,21 @@ class UserSeeder extends Seeder
     {
         $user1 = User::create([
             'nom' => 'TOYI',
-            'prenom'=>'François',
-            'email' =>'toyifrancois@gmail.com',
-            'contact'=>'93516499',
-            'mot_de_passe'=>Hash::make('password')
+            'prenom' => 'François',
+            'email' => 'toyifrancois@gmail.com',
+            'contact' => '93516499',
+            'mot_de_passe' => Hash::make('password'),
         ]);
-        //$user1->assignRole("admin");
+        $user1->assignRole("admin");
 
         $user2 = User::create([
             'nom' => 'TOYI',
-            'prenom'=>'Fabrice',
-            'email' =>'toyifabrice@gmail.com',
-            'contact'=>'97607031',
-            'mot_de_passe' => Hash::make('password')
+            'prenom' => 'Fabrice',
+            'email' => 'toyifabrice@gmail.com',
+            'contact' => '97607031',
+            'mot_de_passe' => Hash::make('password'),
         ]);
-        //$user2->assignRole('editor');
+        $user2->assignRole('editor');
         //
     }
 }
