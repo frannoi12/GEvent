56a67e078f713f355db0d46bd0cd5e1443e5c412
frannoi12 <francoistoyi4@gmail.com>
2023-07-04 08:37:35 +0000

    new

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div>
-                    <a href="{{route('export.pdf')}}" class=" focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Exposer en PDF</a>
+                    <a href="{{route('export.pdf')}}" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Exposer en PDF</a>
                     <a href="#" btn btn-primary>Exporter en excel</a>
                 </div>
                 <div class="p-6 text-gray-900">
